1319190a7d4ddcd4f2ed60dcb98b484457299211
Maxim <RumaBiss@users.noreply.github.com>
2020-09-09 18:43:32 +0300

    Added all files

---
+++ grupa2/src/main/java/Company.java
@@ -0,0 +1,17 @@
+
+public class Company {
+	public Company() {
+	}
+
+	public static void main(String[] args) {
+		Bicycle firstBicycle = new Bicycle();
+		firstBicycle.newBicycle("CrossBike", "Mountain", "Aluminium", "Green", 3680.0F);
+		Bicycle secondBicycle = new Bicycle();
+		secondBicycle.newBicycle("Ardis X-Cross", "Mountain", "Aluminium", "Gray", 2648.0F);
+		System.out.println("________________");
+		System.out.println("    First bicycle:");
+		firstBicycle.showInfoAboutBicycle();
+		System.out.println("    Second bicycle:");
+		secondBicycle.showInfoAboutBicycle();
+	}
+}
